08944bca00937e713621d223a99cce31adf0b6d0
Sarthak <shahifaqeer@gmail.com>
2014-04-04 17:40:54 +0000

    Patching with reverse iperf code

--- src/Reporter.c
+++ src/Reporter.c
@@ -308,11 +308,6 @@ ReportHeader* InitReport( thread_Settings *agent ) {
 #else
         // set start time
         gettimeofday( &(reporthdr->report.startTime), NULL );
-
-	// set next time
-	reporthdr->report.nextTime = reporthdr->report.startTime;
-	TimeAdd( reporthdr->report.nextTime, reporthdr->report.intervalTime );
-
         /*
          * Process the report in this thread
          */
